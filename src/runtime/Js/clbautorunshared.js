f722720c971e0e059eca6573cb115928e4b1218a
captchaaspa-dev <captcha.aspa@gmail.com>
2025-10-08 09:58:52 +0200

    Add files via upload

---
+++ src/runtime/Js/clbautorunshared.js
@@ -0,0 +1,37 @@
+
+
+function colaborasInit(eventObj) {
+    display_insight_infobar();
+}
+
+/**
+ * Creates information bar to display when new message or appointment is created
+ */
+function display_insight_infobar() {
+  Office.context.mailbox.item.notificationMessages.addAsync("fd90eb33431b46f58a68720c36154b4a", {
+    type: "insightMessage",
+    message: "Cree su sala de conversación Talk.",
+    icon: "Icon.16x16",
+    actions: [
+      {
+        actionType: "showTaskPane",
+        actionText: "Crear sala Talk",
+        commandId: get_command_id(),
+        contextData: "{''}",
+      },
+    ],
+  });
+}
+
+/**
+ * Gets correct command id to match to item type (appointment or message)
+ * @returns The command id
+ */
+function get_command_id() {
+  if (Office.context.mailbox.item.itemType == "appointment") {
+    return "MRCS_TpBtn1";
+  }
+  return "MRCS_TpBtn0";
+}
+
+Office.actions.associate("colaborasInit", colaborasInit);
---
+++ src/runtime/Js/clbautorunweb.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// This file contains code only used by autorunweb.html when loaded in Outlook on the web.
+
+Office.initialize = function (reason) {};
